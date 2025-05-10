90985e740e9182e6d72af79e31a3ecc8665b0426
Rhod-Thomas <engineer_rod@hotmail.co.uk>
2025-05-10 13:20:26 +0100

    exception handling added

--- portRead.py
+++ portRead.py
@@ -5,14 +5,20 @@ handle = ser.Serial("/dev/ttyACM0")
 line = ""
 
 while(True):
-    
-    ch = handle.read().decode('utf-8')
-    #print(ch)
-    line += ch
-    #print(line)
+   
+    try:
 
-    if ch == '\n':
-        line = line.replace('\n', '')
-        print(line)
-        line = ""
+        ch = handle.read().decode('utf-8')
+        #print(ch)
+        line += ch
+        #print(line)
+
+        if ch == '\n':
+            line = line.replace('\n', '')
+            print(line)
+            line = ""
+
+    except:
+
+        print("exception handled.")
 
